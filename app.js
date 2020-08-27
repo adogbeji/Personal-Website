79aa723ce9ce4dbd652283d705ea52f875b63c2c
Ben <baka_1993@yahoo.co.uk>
2020-08-27 17:34:55 +0100

    Set border-radius on Contact Form to Zero

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//For Next Time: Start building 'Learn More' button on Homepage!
+// For Next Time: Adjust 'form-container' class on Contact Page to prevent gaps appearing during form submission
 
 app.get("/", function(req, res) {
   res.render("home");
