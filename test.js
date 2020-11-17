3abf1aaa36b6b6d170e01e290a8d62c8f32dd91a
Ben <baka_1993@yahoo.co.uk>
2020-11-17 15:27:12 +0000

    Add overlay to Home Page background image

---
+++ test.js
@@ -0,0 +1,40 @@
+//jshint esversion:6
+
+require("dotenv").config({path: "./config.env"});
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/public`));  // Mounts static files on root route ('/')
+
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+
+app.get("/sitemap.xml", (req, res) => {
+  res.sendFile(__dirname + "/sitemap.xml");
+});
+
+app.get("/robots.txt", (req, res) => {
+  res.sendFile(__dirname + "/robots.txt");
+});
+
+// No Contact Post Route - Emails are being sent through Google Sheets!
+
+const port = process.env.PORT;
+
+app.listen(port, function() {
+  console.log("Listening on port 3000...");
+});
