f82f6360d434f7429224d46a72582938a711a6b2
Ben <baka_1993@yahoo.co.uk>
2020-11-24 14:51:06 +0000

    Change Home Page CSS Selectors

--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 //jshint esversion: 6
 
 $(document).ready(function() {  //ALlows header text to slowly slide into view from the top
-  $(".animate").slideDown(2500, "linear");
+  $(".home-page-animate").slideDown(2500, "linear");
 });
 
 const scrollButton = document.querySelector(".scroll-top");
